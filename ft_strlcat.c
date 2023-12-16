0f2fb025d5ae47ef88759ba98438913500438ae9
Daniel <72256059+ZedXnon@users.noreply.github.com>
2023-12-16 21:22:16 +1030

    added strlcat

---
+++ ft_strlcat.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcat.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dwoidt <dwoidt@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/16 20:55:19 by dwoidt            #+#    #+#             */
+/*   Updated: 2023/12/16 20:55:19 by dwoidt           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+size_t strlcat(char *dest, const char *src, size_t size)
+{
+	size_t	i;
+	size_t	dest_length;
+	
+	i = 0;
+	dest_length = 0;
+	while (dest[dest_length] && dest_length < size)
+        dest_length++;
+	while (src[i] != '\0' && dest_length + i < size - 1)
+	{
+		dest[dest_length + i] = src[i];
+		i++;
+	}
+	if (dest_length < size) {
+        dest[dest_length + i] = '\0';
+    }
+	return (dest_length + ft_strlen(src));
+}
--- libft.h
+++ libft.h
@@ -29,6 +29,7 @@ void	*ft_memset(void *str, int c, size_t n);
 void	ft_bzero(void *s, size_t n);
 void	*ft_memcpy(void *dest, const void *src, size_t n);
 void	*ft_memmove(void *dest, const void *src, size_t n);
-int		ft_strlcpy(char *dst, const char *src, size_t size);
+size_t	ft_strlcpy(char *dst, const char *src, size_t size);
+size_t strlcat(char *dst, const char *src, size_t size);
 
 #endif
