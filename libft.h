3fecd0deaa5ff41cc27ce35ee3e1194e643d0ee7
Daniel <72256059+ZedXnon@users.noreply.github.com>
2023-12-16 20:54:36 +1030

    Added strlcpy

--- libft.h
+++ libft.h
@@ -29,5 +29,6 @@ void	*ft_memset(void *str, int c, size_t n);
 void	ft_bzero(void *s, size_t n);
 void	*ft_memcpy(void *dest, const void *src, size_t n);
 void	*ft_memmove(void *dest, const void *src, size_t n);
+int		ft_strlcpy(char *dst, const char *src, size_t size);
 
 #endif
