0feb20cb437d1d94c403ae54e0086910a206a01a
Daniel <72256059+ZedXnon@users.noreply.github.com>
2023-12-17 20:16:18 +1030

    added function and updated make and header file

--- Makefile
+++ Makefile
@@ -1,9 +1,9 @@
 CC = gcc
 CFLAGS = -Wall -Wextra -Werror
 
-STRING_FUNCS = ft_strlen.c ft_strchr.c ft_toupper.c ft_tolower.c ft_strlcpy.c ft_strlcat.c ft_strrchr.c
+STRING_FUNCS = ft_strlen.c ft_strchr.c ft_toupper.c ft_tolower.c ft_strlcpy.c ft_strlcat.c ft_strrchr.c ft_strncmp.c
 NUM_FUNCS = ft_isdigit.c
-CHAR_FUNCS = ft_isalpha.c ft_isalnum.c ft_isascii.c ft_isprint.c
+CHAR_FUNCS = ft_isalpha.c ft_isalnum.c ft_isascii.c ft_isprint.c ft_memchr.c
 MEMORY_FUNCS = ft_bzero.c ft_memcpy.c ft_memset.c ft_memmove.c
 
 SOURCES = $(STRING_FUNCS) $(NUM_FUNCS) $(CHAR_FUNCS) $(MEMORY_FUNCS)
--- libft.h
+++ libft.h
@@ -29,8 +29,9 @@ void	*ft_memset(void *str, int c, size_t n);
 void	ft_bzero(void *s, size_t n);
 void	*ft_memcpy(void *dest, const void *src, size_t n);
 void	*ft_memmove(void *dest, const void *src, size_t n);
-size_t	ft_strlcpy(char *dst, const char *src, size_t size);
-size_t	strlcat(char *dst, const char *src, size_t size);
-char	*strrchr(const char *string, int character);
+size_t	ft_strlcpy(char *dest, const char *src, size_t size);
+size_t	ft_strlcat(char *dest, const char *src, size_t size);
+char	*ft_strrchr(const char *string, int character);
+void	*ft_memchr(const void *str, int c, size_t n);
 
 #endif
