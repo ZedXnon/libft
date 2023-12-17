9c2afcac62bdb7c43b4c74d4666c080f093b2826
Daniel <72256059+ZedXnon@users.noreply.github.com>
2023-12-17 20:43:49 +1030

    Added strnstr

---
+++ ft_strnstr.c
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strnstr.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dwoidt <dwoidt@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/17 19:14:35 by dwoidt            #+#    #+#             */
+/*   Updated: 2023/12/17 19:14:35 by dwoidt           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
+{
+	size_t	i;
+	size_t	j;
+	size_t	needle_len;
+
+	needle_len = ft_strlen(needle);
+	if (needle_len == 0)
+		return ((char *)haystack);
+	i = 0;
+	while (i < len)
+	{
+		if (haystack[i] == needle[0])
+		{
+			j = 0;
+			while (haystack[i + j] == needle[j] && (i + j) < len)
+			{
+				if (j + 1 == needle_len)
+				{
+					return ((char *)(haystack + i));
+				}
+				j++;
+			}
+		}
+		i++;
+	}
+	return (NULL);
+}
--- libft.h
+++ libft.h
@@ -33,5 +33,6 @@ size_t	ft_strlcpy(char *dest, const char *src, size_t size);
 size_t	ft_strlcat(char *dest, const char *src, size_t size);
 char	*ft_strrchr(const char *string, int character);
 void	*ft_memchr(const void *str, int c, size_t n);
+char	*ft_strnstr(const char *haystack, const char *needle, size_t len);
 
 #endif
