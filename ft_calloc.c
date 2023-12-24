4596a6e794c4cc428ac40d30f6b1b2388ff81e93
ZedXnon <72256059+ZedXnon@users.noreply.github.com>
2023-12-24 12:09:26 +1030

    Fixed norminette errors

--- ft_calloc.c
+++ ft_calloc.c
@@ -12,18 +12,18 @@
 
 #include "libft.h"
 
-void* ft_calloc(size_t num_elements, size_t element_size)
+void	*ft_calloc(size_t num_elements, size_t element_size)
 {
 	size_t	i;
-	size_t total_size = num_elements * element_size;
+	size_t	total_size;
+	void	*memory;
 
 	i = 0;
-	void *memory;
+	total_size = num_elements * element_size;
 	memory = malloc(total_size);
-
 	while (i < total_size)
 	{
-		*((unsigned char*)memory + i) = 0;
+		*((unsigned char *)memory + i) = 0;
 		i++;
 	}
 	return (memory);
