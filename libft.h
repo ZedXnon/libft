c648e2928f86e40528fc58c5dd48616c88e8a446
ZedXnon <72256059+ZedXnon@users.noreply.github.com>
2023-12-24 15:31:38 +1030

    Added ft_strtrim

--- libft.h
+++ libft.h
@@ -40,5 +40,6 @@ char	*ft_substr(char const *s, unsigned int start, size_t len);
 void	*t_calloc(size_t num_elements, size_t element_size);
 char	*strdup(const char *str);
 char	*ft_strjoin(char const *s1, char const *s2);
+char	*ft_strtrim(char const *s1, char const *set);
 
 #endif
