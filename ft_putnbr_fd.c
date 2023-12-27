0fa1686fc4feeac3b15d5fc6faa7d275b7a7c3c3
ZedXnon <72256059+ZedXnon@users.noreply.github.com>
2023-12-28 10:04:04 +1030

    added fd put nbr

--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -1,19 +1,24 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_putendl_fd.c                                    :+:      :+:    :+:   */
+/*   ft_putnbr_fd.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: dwoidt <dwoidt@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/12/25 03:32:05 by dwoidt            #+#    #+#             */
-/*   Updated: 2023/12/25 03:32:05 by dwoidt           ###   ########.fr       */
+/*   Created: 2023/12/25 03:32:11 by dwoidt            #+#    #+#             */
+/*   Updated: 2023/12/25 03:32:11 by dwoidt           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdio.h>
 
-void	ft_putendl_fd(char *s, int fd)
+void	ft_putnbr_fd(int n, int fd)
 {
+	char	*s;
+
+	s = ft_itoa(n);
 	write(fd, s, ft_strlen(s));
-	write(fd, "\n", 1);
 }
