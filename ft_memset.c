fe3fdc736fdc8b1cfc59ac8b60fb7d2c29a4e615
ZedXnon <72256059+ZedXnon@users.noreply.github.com>
2024-01-07 23:49:30 +1030

    Fixed

--- ft_memset.c
+++ ft_memset.c
@@ -14,13 +14,14 @@
 
 void	*ft_memset(void *str, int c, size_t n)
 {
-	unsigned char	*ptr;
+    unsigned char	*ptr;
 
-	ptr = (unsigned char *)str;
-	while (n > 0)
-	{
-		ptr[n - 1] = (unsigned char)c;
-		n--;
-	}
-	return (str);
+    ptr = (unsigned char *)str;
+    while (n > 0)
+    {
+        *ptr = (unsigned char)c;
+        ptr++;
+        n--;
+    }
+    return (str);
 }
